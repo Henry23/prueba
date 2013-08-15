058b0f1e845739564ef1e9a4d3c5334c03716691
Henry23 <gabriel_23hgap@unitec.edu>
2013-08-15 03:03:31 -0700

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Henry
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona per = new Persona("Victor", 130);
+        
+        System.out.println(per.toString());
+    }
+}
